9e32ba26645e8aa8c8b9a906e22e6b72fb4c9660
Felix Otto <dev@felix-otto.de>
2023-05-26 10:50:59 +0200

    FragmentExample: gradle 8.0, compile/target sdk 33, use AndroidX
    Increasing the min sdk to 16 was required to get the fragment wizard
    working in Android Studio 2022.2

--- FragmentExample_start/app/src/main/java/com/example/android/fragmentexample/MainActivity.java
+++ FragmentExample_start/app/src/main/java/com/example/android/fragmentexample/MainActivity.java
@@ -16,7 +16,7 @@
 
 package com.example.android.fragmentexample;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
